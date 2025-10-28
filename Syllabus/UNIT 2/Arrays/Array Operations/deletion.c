c00b16274736191178d8557f81c37532674a50de
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2025-10-28 18:22:33 +0530

    Deletion of element Using Functions

--- Syllabus/UNIT 2/Arrays/Array Operations/deletion.c
+++ Syllabus/UNIT 2/Arrays/Array Operations/deletion.c
@@ -13,6 +13,35 @@
 
 */
 
+void view(int arr[], int size)
+{
+
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+}
+
+void deleter(int index, int size, int arr[])
+{
+  for (int i = index; i < size - 1; i++)
+  {
+    arr[i] = arr[i + 1];
+  }
+}
+
+void indexFinder(int arr[], int size, int *index, int element)
+{
+
+  for (int i = 0; i < size; i++)
+  {
+    if (arr[i] == element)
+    {
+      *index = i;
+      break;
+    }
+  }
+}
 int main()
 {
 
@@ -21,8 +50,7 @@ int main()
   int element;    // Element to delete
   int index = -1; // To store the index of the element to delete(-1 means not found)
 
-
-  //Program header for user readability
+  // Program header for user readability
   printf("\n-------------------------------\n");
   printf("Program to delete array element");
   printf("\n-------------------------------\n");
@@ -31,34 +59,22 @@ int main()
   printf("\nPlease Enter the Size of Array :  ");
   scanf("%d", &size);
 
-
-  //Input array elements from user
+  // Input array elements from user
   printf("\nEnter The Elements of array : ");
   for (int i = 0; i < size; i++)
   {
     scanf("%d", &arr[i]);
   }
 
-  //Display the entered array
+  // Display the entered array
   printf("The Elements of array : ");
-  for (int i = 0; i < size; i++)
-  {
-    printf("%d ", arr[i]);
-  }
-
-  //Ask for the element to delete
+  view(arr, size);
+  // Ask for the element to delete
   printf("\nEnter the element you want to delete: ");
   scanf("%d", &element);
 
   // FINDING INDEX OF ELEMENT
-  for (int i = 0; i < size; i++)
-  {
-    if (arr[i] == element)
-    {
-      index = i;
-      break;
-    }
-  }
+  indexFinder(arr, size, &index, element);
 
   if (index == -1)
   {
@@ -66,21 +82,14 @@ int main()
   }
   else
   {
-
-    for (int i = index; i < size - 1; i++)
-    {
-      arr[i] = arr[i + 1];
-    }
+    deleter(index, size, arr);
     size--;
     printf("\n-------------------------------\n");
     printf("Deleted element %d at index %d\n", element, index);
     printf("\n-------------------------------\n");
 
     printf("The new array is :\n");
-    for (int i = 0; i < size; i++)
-    {
-      printf("%d ", arr[i]);
-    }
+    view(arr, size);
   }
   return 0;
 }
