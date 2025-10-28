9ca57cf97936089931d1edd474096fb645b7bca4
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2025-10-28 19:18:41 +0530

    BInary Search For HardCoded Sorted Array

--- Syllabus/UNIT 2/Arrays/Array Operations/binarySearch.c
+++ Syllabus/UNIT 2/Arrays/Array Operations/binarySearch.c
@@ -1,13 +1,10 @@
 #include <stdio.h> //Include standard input/output library
-
 // Function to perform Binary search on an array
 int binarySearch(int arr[], int size, int element)
 {
-
   int low = 0;
   int high = size - 1;
   int mid;
-
   // START SEARCHING
 
   while (low <= high)
@@ -29,12 +26,10 @@ int binarySearch(int arr[], int size, int element)
 
   return -1;
 }
-
 void main()
 {
   // Declare and initialize a sorted array in ascending order
   int sortedArr[] = {2, 5, 8, 12, 15, 19, 23, 27, 31, 36};
-
   // Element to search for in the array
   int element = 5;
   // Calculate the number of elements in the array
